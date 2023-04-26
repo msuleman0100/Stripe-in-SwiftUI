a6b336a1282b719213118279f1a9815bd202ea46
msuleman0100 <msuleman0100@gmail.com>
2023-04-26 11:13:11 +0500

    project added

---
+++ Stripe-in-SwiftUI/Stripe_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stripe_in_SwiftUIApp.swift
+//  Stripe-in-SwiftUI
+//
+//  Created by Muhammad Suleman on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Stripe_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
